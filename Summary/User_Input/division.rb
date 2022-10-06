e186d100d2f5f3acb770c1479040d1f429181ccc
Benjamin Walker <benjamin.q.walker@gmail.com>
2022-10-06 11:48:57 -0500

    User input exercises fully complete

--- Summary/User_Input/division.rb
+++ Summary/User_Input/division.rb
@@ -17,19 +17,20 @@ end
 loop do
   puts ">> Please enter the denominator:    "
   denom = gets.chomp
-  if denom.to_i == 0
-    puts ">> Invalid input. A denominator of 0 is not allowed.    "
-  elsif (valid_number?(denom) && denom!=0)
+  if (valid_number?(denom) && denom!="0")
     break
+  elsif denom == "0"
+    puts ">> Invalid input. A denominator of 0 is not allowed.    "
   else
     puts ">> Enter an integer instead"
   end
 end
 
-#until denom != 0
-#  puts ">> Please enter the denominator:"
-#  denom = gets.chomp
-#end
+# I'm trying not to be so so frustrated, this exercise took at least an hour
+# it came down to me not understanding that the user inputting 0 is actually taken
+# as "0" (a string) so when I set up conditionals to catch !=0 they actually ran
+# true when the user input 0 because "0" is indeed NOT equal to 0. FUCK
+# I also tried writing lots of different loops
 
-puts denom
-#puts "Sure thing. #{num} / #{denom} is #{num.to_i / denom.to_i}"
+
+puts "Sure thing. #{num} / #{denom} is #{num.to_i / denom.to_i}"
