3311b9faafea2568d16e471c5f0f4b55a603be23
Benjamin Walker <benjamin.q.walker@gmail.com>
2022-11-25 11:42:07 -0600

    L6 tictactoe initialization

---
+++ RB101/L6/tictactoe.rb
@@ -0,0 +1,17 @@
+=begin
+1. Display the initial empty 3x3 board.
+  Probably keep the values of the board in a 9-element array, in "read-order" 123 top, 456 middle, 789 bottom. Could display them in 3x3 (2-D) format
+2. Ask the user to mark a square.
+  Could list the places as numbers until they're marked with X or O
+3. Computer marks a square.
+4. Display the updated board state.
+5. If winner, display winner.
+  For my array idea, this would be pretty hard to capture; there'd be a lot of winning combinations, how many?
+    3 accross, 3 down, 2 diagonals (wow, WAY fewer than I thought, ha)
+    
+6. If board is full, display tie.
+7. If neither winner nor board is full, go to #2
+8. Play again?
+9. If yes, go to #1
+10. Good bye!
+=end
