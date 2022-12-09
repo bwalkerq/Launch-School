fbeb4df56a3e527c131bade135deb99419b33f50
Benjamin Walker <benjamin.q.walker@gmail.com>
2022-12-09 11:06:02 -0600

    21 game 1st draft complete, submitted for feedback

--- RB101/L6/21_game.rb
+++ RB101/L6/21_game.rb
@@ -1,6 +1,8 @@
 CARD_VALUES = { jack: 10, queen: 10, king: 10, ace: 11, ace_as_one: 1 }
 (2..10).each { |x| CARD_VALUES[x] = x }
-p CARD_VALUES
+
+player_score = 0
+dealer_score = 0
 
 def prompt(msg)
   puts "==> #{msg}"
@@ -9,7 +11,7 @@ end
 def initialize_deck
   deck = []
   4.times do
-    (1..10).each { |x| deck << x }
+    (2..10).each { |x| deck << x }
     deck += [:jack, :queen, :king, :ace]
   end
   deck
@@ -63,69 +65,105 @@ def show_current_state(hand, dlr_hand)
   calculate_score(hand) # this is here to catch
   prompt "Your hand is currently: #{hand.join(', ')}"
   prompt "Your SCORE is ** #{calculate_score(hand)} **"
+  puts ""
   prompt "My hand is currently: #{dlr_hand.join(', ')}"
   prompt "My SCORE is ** #{calculate_score(dlr_hand)} **"
 end
 
-def player_turn(dck, hand, dlr_hand, dlr_score)
+def player_turn(dck, hand, dlr_hand)
   loop do
+    system 'clear'
     show_current_state(hand, dlr_hand)
     break if busted?(hand)
+    puts ""
     prompt "Would you like to Hit or Stay?"
     answer = gets.chomp.downcase
     break if answer.start_with?('s')
     deal_a_card(dck, hand)
   end
-  show_current_state(hand, dlr_hand)
-
+  # show_current_state(hand, dlr_hand)
 end
 
-def dealer_turn(dck, hand, dlr_hand, plyr_score)
-  until calculate_score(dlr_hand) > 17
+def dealer_turn(dck, hand, dlr_hand)
+  until (calculate_score(dlr_hand) >= 17) ||
+        ((calculate_score(hand) <=> calculate_score(dlr_hand)) == -1)
+    prompt "I'll take another card"
     deal_a_card(dck, dlr_hand)
     show_current_state(hand, dlr_hand)
+    prompt "==============================="
   end
-
 end
 
-def declare_winner(hand,dlr_hand)
-  # I should complete the tictactoe refactor first, then work on this
-  # About to work on this, instructions say that one method should return the winner,
-  # and another method should display the prompt about the winner
-  # instructions say that each method should do only one thing
-  # which prompted me to skim my methods and see that there are clearly many which do
-  # more than just one thing, so I should probably refactor this a lot
-  winner = nil
+def determine_winner(hand, dlr_hand)
+  if calculate_score(hand) >= calculate_score(dlr_hand)
+    "player"
+  else
+    "dealer"
+  end
+end
+
+def declare_winner(winner)
+  case winner
+  when "player"
+    message = "you, the mighty player"
+  when "dealer"
+    message = "me, the famous Don Chu"
+  end
+  prompt "Aha! The winner of this game is #{message}"
+end
+
+# def adjust_score(plyr_hand, dlr_hand, plyr_score, dlr_score)
+#   case determine_winner(plyr_hand, dlr_hand)
+#   when "player"
+#     plyr_score += 1
+#   when "dealer"
+#     dlr_score += 1
+#   end
+# end
+
+def play_again_prompt(plyr_score, dlr_score)
+  prompt "Number of times you have won: #{plyr_score}"
+  prompt "Number of times I, the great Don Chu Luffloosen, have won: #{dlr_score}."
+  prompt "Would you like to try your luck again? (Y to play again)"
+  answer = gets.chomp.downcase
+  answer.start_with?('y')
 end
 
 # Gameplay
-prompt "Welcome to 21. Must be 21 to play... Wait, what?"
-prompt "I'm a little new at this, but let's get started. I'll be your friendly dealer, Don Chu Luffloosen."
-player_score = 0
-dealer_score = 0
+prompt "Welcome to the 21 game!"
+prompt "I'll be your friendly dealer, Don Chu Luffloosen."
 
 loop do
   deck = initialize_deck
   player_hand = initial_deal(deck)
   dealer_hand = initial_deal(deck)
-  player_turn(deck, player_hand, dealer_hand, dealer_score)
+  player_turn(deck, player_hand, dealer_hand)
   # 4. If player bust, dealer wins.
-  if busted?(hand)
+  if busted?(player_hand)
     prompt "Sorry, you busted! I have won!"
-    dlr_score += 1
+    dealer_score += 1
   end
   # 5. Dealer turn: hit or stay
   # - repeat until total >= 17
-  dealer_turn(deck, player_hand, dealer_hand, player_score)
+  dealer_turn(deck, player_hand, dealer_hand) if !busted?(player_hand)
   # 6. If dealer bust, player wins.
   if busted?(dealer_hand)
     prompt "Drat, I have indeed busted. Well done."
-    plyr_score += 1
-    break
+    player_score += 1
   end
   # 7. Compare cards and declare winner.
-  declare_winner if (!busted?(dealer_hand) && !busted?(player_hand))
-  prompt "Number of times you have won: #{player_score}"
-  prompt "Number of times I, the great Don Chu, have won: #{dealer_score}."
+  if !busted?(dealer_hand) && !busted?(player_hand)
+    declare_winner(determine_winner(player_hand, dealer_hand))
+    # adjust_score(player_hand, dealer_hand, player_score, dealer_score)
+    case determine_winner(player_hand, dealer_hand)
+    when "player"
+      player_score += 1
+    when "dealer"
+      dealer_score += 1
+    end
+  end
+  break unless play_again_prompt(player_score, dealer_score)
+  prompt "Very well, let's have another round!"
 end
-
+prompt "I have enjoyed this immensely. Farewell!"
+prompt "==============================="
