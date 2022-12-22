2909c69794aa0cfc4b4ae0563d891b51ca6fbbb1
benjaminwalker on Wally's <benjamin.q.walker@gmail.com>
2022-12-22 08:59:11 -0600

    Small easy 8 debug double_consonants

--- RB101/Small/Small_Easy_8.rb
+++ RB101/Small/Small_Easy_8.rb
@@ -46,20 +46,127 @@ p leading_substrings('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
 
 def substrings(string)
   result_array = []
-  initial_subs = []
+  initial_substrings = []
   string.length.times do |index|
-    initial_subs << string.slice(index-1, -1)
+    initial_substrings << string.slice(index, string.length)
   end
-  p initial_subs
-  result_array = initial_subs.each {|substring| leading_substrings(substring)}
+  initial_substrings.each {|substring| result_array += leading_substrings(substring)}
   result_array
 end
 
-p substrings('abcde')
-# == [
-#   'a', 'ab', 'abc', 'abcd', 'abcde',
-#   'b', 'bc', 'bcd', 'bcde',
-#   'c', 'cd', 'cde',
-#   'd', 'de',
-#   'e'
-# ]
+p substrings('abcde') == [
+  'a', 'ab', 'abc', 'abcd', 'abcde',
+  'b', 'bc', 'bcd', 'bcde',
+  'c', 'cd', 'cde',
+  'd', 'de',
+  'e'
+]
+
+def palindromes(string)
+  substrings(string).select do |substring|
+    substring == substring.reverse && substring.length > 1
+  end
+end
+
+p palindromes('abcd') == []
+p palindromes('madam') == ['madam', 'ada']
+p palindromes('hello-madam-did-madam-goodbye') == [
+  'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
+  'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
+  '-madam-', 'madam', 'ada', 'oo'
+]
+p palindromes('knitting cassettes') == [
+  'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
+]
+
+def fizzbuzz(start_num, end_num)
+  (start_num..end_num).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+  end
+end
+
+fizzbuzz(1, 15) # -> 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
+
+def repeater(string)
+  new_string = []
+  string.chars.each do |letter|
+    new_string << letter << letter
+  end
+  new_string.join
+end
+
+p repeater('Hello') == "HHeelllloo"
+p repeater("Good job!") == "GGoooodd  jjoobb!!"
+p repeater('') == ''
+
+def double_consonants (string)
+  new_string = []
+  string.chars.each do |letter|
+    if letter =~ /[^a-zA-Z&&aeiou]/
+      new_string << letter << letter
+    else
+      new_string << letter
+    end
+  end
+  new_string.join
+end
+
+p double_consonants('String') #== "SSttrrinngg"
+p double_consonants("Hello-World!") #== "HHellllo-WWorrlldd!"
+p double_consonants("July 4th") == "JJullyy 4tthh"
+p double_consonants('') == ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
