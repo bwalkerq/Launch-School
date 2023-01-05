30d6d524dfd71070f443b1bdbdc17f3a29929e95
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-01-05 11:49:21 -0600

    Small Medium 2 complete

--- RB101/Small/Small_Medium_2.rb
+++ RB101/Small/Small_Medium_2.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
 
 def longest_sentence(string)
-  longest = ""
+  longest = ''
   string.split(/[?.!]/).each do |sentence|
     longest = sentence if sentence.split.count > longest.split.count
   end
-  longest.gsub!("\n", " ")
+  longest.gsub!("\n", ' ')
   "The longest sentence is #{longest.split.count} words. It is: #{longest}."
 end
 
@@ -48,9 +49,9 @@ earth."
 
 p longest_sentence(getty)
 
-BLOCKS_HASH = { B: "O", X: "K", D: "Q", C: "P", N: "A",
-                G: "T", R: "E", F: "S", J: "W", H: "U",
-                V: "I", L: "Y", Z: "M" }
+BLOCKS_HASH = { B: 'O', X: 'K', D: 'Q', C: 'P', N: 'A',
+                G: 'T', R: 'E', F: 'S', J: 'W', H: 'U',
+                V: 'I', L: 'Y', Z: 'M' }
 
 # How to organize the blocks?
 # Hash, or perhaps nested array
@@ -59,26 +60,22 @@ BLOCKS_HASH = { B: "O", X: "K", D: "Q", C: "P", N: "A",
 # Could also set up hash key:value and then take the keys, values, put into two arrays and as the string
 # searches for each block, removes it from the hash, returns false if block missing from hash when needed
 
-
 def block_word?(string)
   blocks = BLOCKS_HASH.clone
   letters = string.upcase.chars
   letters.each do |letter|
     case
-    when blocks.keys.include?(letter.to_sym)
-      blocks.delete(letter.to_sym)
-    when blocks.values.include?(letter)
-      blocks.delete(blocks.key(letter))
-    else
-      return false
+    when blocks.keys.include?(letter.to_sym) then blocks.delete(letter.to_sym)
+    when blocks.values.include?(letter) then blocks.delete(blocks.key(letter))
+    else return false
     end
   end
   true
 end
 
-BLOCKS = %w(BO XK DQ CP NA GT RE FS JW HU VI LY ZM).freeze
+BLOCKS = %w[BO XK DQ CP NA GT RE FS JW HU VI LY ZM].freeze
 
-def block_word?(string)
+def block_word2?(string)
   up_string = string.upcase
   BLOCKS.none? { |block| up_string.count(block) >= 2 }
 end
@@ -95,17 +92,15 @@ p block_word?('jest') #== true
 p block_word?('BQP') #== true
 
 def letter_percentages(string)
-  hash = {lowercase: 0, uppercase: 0, neither: 0}
+  hash = { lowercase: 0, uppercase: 0, neither: 0 }
   string.chars.each do |character|
-    if character =~ /[a-z]/
-      hash[:lowercase] += 1
-    elsif character =~ /[A-Z]/
-      hash[:uppercase] += 1
-    else
-      hash[:neither] += 1
+    case character
+    when /[a-z]/ then hash[:lowercase] += 1
+    when /[A-Z]/ then hash[:uppercase] += 1
+    else hash[:neither] += 1
     end
   end
-  hash.each do |k,v|
+  hash.each do |k, v|
     hash[k] = (v.to_f / string.size * 100).round(1)
   end
   hash
@@ -114,7 +109,7 @@ end
 p letter_percentages('abCdef 123') == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
 p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
 p letter_percentages('123') == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
-p letter_percentages "abcdefGHI"
+p letter_percentages 'abcdefGHI'
 
 # 31 minutes
 # distracted as fuck throughout solving this problem, working in a coffee shop next
@@ -150,14 +145,14 @@ def balanced?(string)
   string.each_char do |char|
     count += 1 if char == '('
     count -= 1 if char == ')'
-    break if count < 0
+    break if count.negative?
   end
   count.zero?
 end
 
 # 14 minutes, several minutes spent just realizing that this is a parity problem that
 # can be addressed with counting pairs, essentially. Feeling stoked that I figured that out.
-# I made a long comment on Maria Milosh's solution, wich is very different than mine, and uses
+# I made a long comment on Maria Milosh's solution, which is very different than mine, and uses
 # the idea of a "stack" like how we did before
 
 p balanced?('What (is) this?') == true
@@ -172,7 +167,6 @@ p balanced?('What ((is))) up(') == false
 # triangle
 # I don't quite know how to do a sampling or whatever where it takes one of the elements
 # and compares it to the other two, like what's necessary for the invalid case
-# 
 
 def triangle(x, y, z)
   return :invalid if x >= (y + z) || y >= (x + z) || z >= (x + y)
@@ -193,16 +187,180 @@ p triangle(3, 4, 5) == :scalene
 p triangle(0, 3, 3) == :invalid
 p triangle(3, 1, 1) == :invalid
 
+def tri_angle(x, y, z)
+  angles = [x, y, z]
+  return :invalid if angles.inject(:+) != 180 || angles.include?(0)
+  case
+  when angles.include?(90) then return :right
+  when angles.any? {|angle| angle > 90} then return :obtuse
+  end
+  :acute
+end
 
+p "angles"
+p tri_angle(60, 70, 50) == :acute
+p tri_angle(30, 90, 60) == :right
+p tri_angle(120, 50, 10) == :obtuse
+p tri_angle(0, 90, 90) == :invalid
+p tri_angle(50, 50, 50) == :invalid
 
+# 9.5 minutes
+# cake
 
+# Friday the 13ths holy hell
+# the number of friday the 13th depends on the start day of the year, so there are 7 options
+# if the year is a leap year (divisible by 4) there are 7 other options, so 14 total
+p 365 % 7 == 1
+# so each year increments the start day by one day; 2023 started on a Sunday, 2024 will start
+# on a Monday. A leap year jumps two start days ahead, so 2025 (the year preceeding a leap year)
+# will start on a Wednesday. then Th, Fri, Sat, then Monday...
+# There has got to be a better way to do this.
+# Yes, my way of doing this was very mathy; there is a more comp-sci way of approaching this
+# with the DATE Class, along with methods like #friday? (I'm glad I looked at the hint)
 
+# algorithm:
+# given a year, go through the months and check if each 13th is friday. If so, increment
+# a counter, return the counter at the end
 
+require 'date'
+def friday_13th(year)
+  fridays = 0
+  (1..12).each do |month|
+    fridays += 1 if Date.new(year, month, 13).friday?
+  end
+  fridays
+end
 
+p "fridays"
+p friday_13th(2015) == 3
+p friday_13th(1986) == 1
+p friday_13th(2019) == 2
+# (2000..2100).each do |year|
+#   p friday_13th(year)
+# end
 
+# 21 minutes - I'm surprised I figured this out so quickly, even with the hint.
+# But my idea worked great, and I am pretty stoked that I figured out how to work with
+# the date class, especially since it requires a "require 'date'" statement, and then each
+# new date has to be initialized, and I had to learn a new format. Pretty cool!
 
+# featured number, x, must be:
+# - x % 7 == 0
+# - x.odd? == true
+# - no repeated digits in the number (this is what limits the total number of featured)
+# algorithm
+# takes input, starts to increment until all the reqs are met, returns that number
+# perhaps first, if the number is too large, returns error message
 
+def featured(num)
+  error_message = "seat's taken"
+  loop do
+    num += 1
+    return error_message if num >= 10_000_000_000 # can't have unique digits if num is 11 digits
+    next unless num % 7 == 0 and num.odd?
+    return num if num.to_s.chars == num.to_s.chars.uniq
+  end
+end
 
+# 10.5 minutes, spent a long time trying to brute force the last featured number, until I
+# realized that it doesn't matter that much, just so that I had an upper bound that was
+# somewhat close.
+# Looking at Daniel Chae's solution, I see that there's a #find method that he uses for a
+# range:
+# def featured(n)
+#   (n+1..9_876_543_201).find { |x| x.odd? && x % 7 == 0 && x.digits.size == x.digits.uniq.size } || "No featured number > n"
+# end
+# note that I dislike his one-liner obsession.
+# but that #find method is sweet
+
+p "featured"
+p featured(12) == 21
+p featured(20) == 21
+p featured(21) == 35
+p featured(997) == 1029
+p featured(1029) == 1043
+p featured(999_999) == 1_023_547
+#p featured(999_999_987) == 1_023_456_987
+#p featured(9999999999) # -> There is no possible number that fulfills those requirements
+
+# can use spaceship operators
+# input: array
+# output: same array, sorted from least to greatest
+# each element compared with its right neighbor, if element is greater than neighbor, swap
+# places
+# could do the thing n times, but that might do unnecessary iterations
+# I could keep count of swaps with a counter; if the counter is zero at the end of a pass,
+# then break from the loops
+
+def bubble_sort_draft!(array)
+  n = array.size
+  (n-1).times do
+  index = 0
+    until index > (array.size - 1) do
+      case array[index] <=> array[index + 1]
+      when 1
+        temp = array[index]
+        array[index] = array[index + 1]
+        array[index + 1] = temp
+      end
+      index += 1
+    end
+  end
+  array
+end
+
+def bubble_sort!(array)
+  loop do
+    swapped = false
+    0.upto(array.size - 2) do|n|
+      next if array[n] <= array[n+1]
+      array[n], array[n + 1] = array[n + 1], array[n]
+      swapped = true
+    end
+    break unless swapped
+  end
+  array
+end
+
+array = [5, 3]
+p bubble_sort!(array)
+p array == [3, 5]
+
+array = [6, 2, 7, 1, 4]
+bubble_sort!(array)
+p array == [1, 2, 4, 6, 7]
+
+array = %w(Sue Pete Alice Tyler Rachel Kim Bonnie)
+bubble_sort!(array)
+p array == %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
+
+# 38 minutes, not my best go; I spent at least half the time using Array#map! and trying
+# to debug it. later i just used an until loop to accomplish the same thing without the hot
+# mess of referencing the array while I'm mutating it. oy vey.
+# Damn their solution uses a much more elegant way (a "ruby idiom", apparently) of swapping
+# two variables:
+# a, b = b, a
+# also theirs is much nicer with two loops, and one that terminates after there are no swaps;
+# they do a better version of my counter idea, which is storing the value of swapped as
+# a boolean, so if swapped is ever false after inspecting all the elements, then it can break
+# out of the loop
+# after looking at their solution, I rewrote it without copying it or trying to memorize
+# it first; gives me the chance to internalize what they did, and I get a feel for the solution
+# path
+
+# mathy!
+def sum_square_difference(n)
+  (1..n).inject(:+) ** 2 - (1..n).map{ |x| x ** 2 }.inject(:+)
+end
+
+p sum_square_difference(3) == 22
+# -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+p sum_square_difference(10) == 2640
+p sum_square_difference(1) == 0
+p sum_square_difference(100) == 25164150
+
+# 4 min
+# I learned that I can't use #map! on a range (1..n) but I CAN use #map. Good to know.
 
 
 
