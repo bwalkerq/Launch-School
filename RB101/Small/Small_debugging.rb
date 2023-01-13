4aac3961cb1abd66c7a89e5a831089086712f846
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-01-13 09:15:41 -0600

    Small Debugging complete

---
+++ RB101/Small/Small_debugging.rb
@@ -0,0 +1,340 @@
+def decrease(counter)
+  counter - 1
+end
+
+counter = 10
+
+counter.times do
+  puts counter
+  counter = decrease(counter)
+end
+
+puts 'LAUNCH!'
+
+def shout_out_to(name)
+  new = name.chars.map { |c| c.upcase! }
+  name = new.join
+  puts 'HEY ' + name
+end
+# I changed #each to #map because #each returns nil, and we wanted the new string returned
+# however, I see now that all we were doing was upcasing a string, so I could have just done
+# away with the block, and called #upcase! on the string itself
+
+shout_out_to('you') # expected: 'HEY YOU'
+
+def valid_series?(nums)
+  return false if nums.sum != 47
+
+  odd_count = nums.count { |n| n.odd? }
+  odd_count == 3
+end
+
+p valid_series?([5, 6, 2, 7, 3, 12, 4, 8])        # should return true
+p valid_series?([1, 12, 2, 5, 16, 6])             # should return false
+p valid_series?([28, 3, 4, 7, 9, 14])             # should return false
+p valid_series?([20, 6, 9, 4, 2, 1, 2, 3])        # should return true
+p valid_series?([10, 6, 19, 2, 6, 4])             # should return false
+
+def reverse_sentence(sentence)
+  words = sentence.split(' ')
+  reversed_words = []
+
+  i = 0
+  while i < words.length
+    reversed_words.prepend(words[i])
+    i += 1
+  end
+
+  reversed_words.join(' ')
+end
+
+p reverse_sentence('how are you doing')
+# expected output: 'doing you are how'
+
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
+
+deck = { :hearts   => cards.dup,
+         :diamonds => cards.dup,
+         :clubs    => cards.dup,
+         :spades   => cards.dup }
+
+def score(card)
+  case card
+  when :ace   then 11
+  when :king  then 10
+  when :queen then 10
+  when :jack  then 10
+  else card
+  end
+end
+
+# Pick one random card per suit
+
+player_cards = []
+deck.keys.each do |suit|
+  cards = deck[suit]
+  cards.shuffle!
+  player_cards << cards.pop
+end
+p player_cards
+p deck
+
+# Determine the score of the remaining cards in the deck
+
+sum = deck.reduce(0) do |sum, (_, remaining_cards)|
+  remaining_cards.map! do |card|
+    score(card)
+  end
+
+  sum += remaining_cards.sum
+end
+
+puts sum
+# nailed it
+
+
+def move(n, from_array, to_array)
+  n.times {to_array << from_array.shift}
+  # move(n - 1, from_array, to_array)
+end
+
+# Example
+
+todo = ['study', 'walk the dog', 'coffee with Tom']
+done = ['apply sunscreen', 'go to the beach']
+
+move(2, todo, done)
+
+p todo # should be: ['coffee with Tom']
+p done # should be: ['apply sunscreen', 'go to the beach', 'study', 'walk the dog']
+
+# this runs into an infinite recursive loop as written, take out the recusion since it's
+# not necessary
+
+def neutralize(sentence)
+  words = sentence.split(' ')
+  words.reject! do |word|
+    negative?(word)
+  end
+
+  words.join(' ')
+end
+
+def negative?(word)
+  [ 'dull',
+    'boring',
+    'annoying',
+    'chaotic'
+  ].include?(word)
+end
+
+puts neutralize('These dull boring cards are part of a chaotic board game.')
+# Expected: These cards are part of a board game.
+# Actual: These boring cards are part of a board game.
+
+# right away I see this is that error that burned me, at some point, I can feel it, like I can
+# feel common chain rule errors with derivatives or something. It just feels clase. As soon
+# as I saw this I knew it had to do with iterating over a collection while mutating the collection
+# which is asking for trouble. As a word is deleted, the other elements are shifted left, and
+# the #each moves to next index, skipping a word. very classic.
+# With this, I'd rather iterate through the negative words and delete those rather than #each
+# through the sentence. Actually, I'd rather just select the words that aren't negative--actually,
+# there's a method for that, #reject the words that ARE in the negative words group
+
+password = nil
+
+def set_password
+  puts 'What would you like your password to be?'
+  new_password = gets.chomp
+  password = new_password # The variable password here is different than the one outside the block
+end
+
+def verify_password(password_argument)
+  puts '** Login **'
+  print 'Password: '
+  input = gets.chomp
+
+  if input == password_argument
+    puts 'Welcome to the inside!'
+  else
+    puts 'Authentication failed.'
+  end
+end
+
+# if !password
+#   password = set_password
+# end
+#
+# verify_password(password)
+
+# I fucking buzzsawed this
+
+def valid_integer?(string)
+  string.to_i.to_s == string
+end
+
+def guess_number(max_number, max_attempts)
+  winning_number = (1..max_number).to_a.sample
+  attempts = 0
+
+  loop do
+    attempts += 1
+    break if attempts > max_attempts
+
+    input = nil
+    until valid_integer?(input)
+      print 'Make a guess: '
+      input = gets.chomp
+    end
+
+    guess = input.to_i
+
+    if guess == winning_number
+      puts 'Yes! You win.'
+      break #overlooked the edge case where we guess correctly in less than 3 guesses
+    else
+      puts 'Nope. Too small.' if guess < winning_number
+      puts 'Nope. Too big.'   if guess > winning_number
+
+    end
+  end
+end
+
+#guess_number(10, 3)
+
+# mostly buzz saw
+
+
+# Term frequency - inverse document frequency:
+# A measure of how important a term is to a document in a collection of documents
+# (the importance increases proportionally to the term frequency in the document,
+# but decreases with the frequency of the word across documents)
+
+def tfidf(term, document, documents)
+  tf(term, document) * idf(term, documents)
+end
+
+# Term frequency (simple version):
+# the number of times a term occurs in a document
+
+def tf(term, document)
+  document.split(/[\s,.-]/).count { |word| word.downcase == term }
+
+end
+
+# Inverse document frequency:
+# measure of how much information the word provides,
+# based on the number of documents in which it occurs
+# (the rarer it is, the more information it provides)
+
+def idf(term, documents)
+  number_of_documents = documents.length # working as expected
+  number_of_documents_with_term = documents.count { |d| tf(term, d) > 0 } # working as expected
+  Math.log(number_of_documents.fdiv(number_of_documents_with_term)) # round this rather than return 0
+end
+
+# Very simple example
+
+document1 = "Schrödinger's cat is a thought experiment often featured in discussions of the interpretation of quantum mechanics. The Austrian physicist Erwin Schrödinger devised " +
+  "it in 1935 as an argument against the Copenhagen interpretation of quantum mechanics, which states that an object in a physical system can simultaneously exist in all possible configurations, " +
+  "a state called quantum superposition, and only observing the system forces the object into just one of those possible states. Schrödinger disagreed with this interpretation. In particular, " +
+  "quantum superposition could not work with larger objects. As an illustration, he presented a scenario with a cat in a sealed box, whose fate was linked to a subatomic event that may or may not occur. " +
+  "In a quantum superposed state of the subatomic particles, the cat would be both alive and dead, until someone opened the box and observed it."
+
+document2 = "The domestic cat is a small, furry, carnivorous mammal. The term cat can, however, also refer to wild cats, which include animals like lions, tigers and leopards. " +
+  "Cats are claimed to have a lower social IQ than dogs but can solve more difficult cognitive problems and have a longer-term memory. International Cat Day is celebrated on August 8. " +
+  "Famous cats include Schrödinger's cat as well as Pudding and Butterscotch, which occur in some of the Launch School assignments."
+
+document3 = "One of the core values that sets Launch School apart from some other coding schools out there is our emphasis on Mastery-based Learning. If the key to becoming a competent and confident Software Engineer " +
+  "is deep understanding of first principles, then the key to acquiring that understanding is through Mastery-based Learning. The core of Mastery-based Learning is replacing time with mastery. There's no graduation, " +
+  "but a continual learning journey of micro-improvements. At Launch School, we're not trying to catch a wave or take advantage of a surge in demand. Instead, we're trying to focus on things that'll be useful to you for decades to come, " +
+  "such as a systematic problem-solving approach or learning how to deconstruct a programming language or building sound mental representations of how web application work. Everything we're trying to do at " +
+  "Launch School is with an eye towards sustainable studying habits and building skills for a long-term career."
+
+documents = [document1, document2, document3]
+
+# The higher the tf-idf score of a term for a document, the more informative
+# it is for that document.
+# E.g. when someone searches for the term 'cat' in your document collection,
+# you want to return document1 and document2, but not document3.
+# For the term 'quantum mechanics', on the other hand, you only want to return document1.
+
+p "outputs:"
+puts tfidf("cat", document1, documents) # ~ 1.2
+puts tfidf("cat", document2, documents) # ~ 1.6
+puts tfidf("cat", document3, documents) # 0
+
+puts tfidf("quantum", document1, documents) # ~ 5.5
+puts tfidf("quantum", document2, documents) # 0
+puts tfidf("quantum", document3, documents) # 0
+
+puts tfidf("mastery", document1, documents) # 0
+puts tfidf("mastery", document2, documents) # 0
+puts tfidf("mastery", document3, documents) # ~ 4.4
+
+puts tfidf("some", document1, documents) # 0
+puts tfidf("some", document2, documents) # ~ 0.4
+puts tfidf("some", document3, documents) # ~ 0.4
+
+# opposite of a buzzsaw
+# this took forever, at least an hour. I got super burned on this division, holy hell
+# the / operator returns an integer and for something like Math#log a float is needed
+# so using Numeric#fdiv is exactly for that, holy shit
+# The takeaway here is that any time I'm looking at a bunch of zeros, it's probably a
+# float/rounding thing, or some other mathy formatting thing
+
+
+arr = ["9", "8", "7", "10", "11", "12", "20"]
+p arr.map { |x| x.to_i }.sort do |x, y|
+  y.to_i <=> x.to_i
+end
+
+p arr.sort { |a ,b| b.to_i <=> a.to_i }
+p(
+  arr.sort do |x, y|
+    y.to_i <=> x.to_i
+  end
+)
+# Expected output: ["11", "10", "9", "8", "7"]
+# Actual output: ["10", "11", "7", "8", "9"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
