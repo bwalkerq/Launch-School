387b28fa3d14a421412e3928fd7a1731d9e5b9ff
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-02-02 14:53:14 -0600

    119 interview practice Codewars document

---
+++ RB101/Assessments/Interview Practice/Codewars problems.rb
@@ -0,0 +1,393 @@
+
+# from https://docs.google.com/document/u/1/d/1usQUJQFr6PGVo3ZWgMi3nVtDRdeUuOUNRtZPtSKkYuE/mobilebasic#h.x956pbnpkojz
+
+
+# Live coding with Devin 1/31
+# // Task
+# // Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+# // For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+# // With list [20,37,20,21] and number 1, the result would be [20,37,21].
+
+=begin
+PROBLEM
+
+input: an array of integers, and an integer
+output: a new array of integers
+
+explicit rules: go through the array and remove any values that occur more than n times
+implicit rules: all positive array values and  pos values of n
+
+Questions:
+
+Mental Model:
+N represents a counter limit, iterate through the list, after a number is counted n times, all other occurances are removed
+
+EXAMPLES ok
+
+DATA / ALGORITHM
+arrays
+
+# set up the empty output_array
+# initialize u_array to hold the unique values
+iterate through unique values array with Each
+  # set a counter to zero
+  iterate through original array,
+    If num matches the unique value AND counter is less than two, shovelling to the output array the occurance of the first two numbers, increment counter,
+    after counter reaches 2, break
+  else next value
+return the output array
+
+CODE
+=end
+
+def delete_nth(array, n)
+  output_array = []
+  u_hash = {}
+  array.uniq.each do |u|
+    u_hash[u] = 0
+  end
+  array.each do |num|
+    counter = 0
+    u_hash.each do |k,v|
+      if v < n && k == num
+        output_array << num
+        u_hash[k] += 1
+      elsif v == n && k == num
+        break
+      else
+        next
+      end
+    end
+  end
+  output_array
+end
+# this was sick because I satisfied the test cases, but I knew that I was not
+# meeting the requirements, so I revamped my algorithm
+
+p (delete_nth([20,37,20,21], 1))  #== [20,37,21]
+p (delete_nth([1,7, 2, 1,3,3,7,2,2,2,2], 3))  #== [1, 1, 3, 3, 7, 2, 2, 2]
+p delete_nth([2,4,4,4,5,4,5,4,6,4,6,3,4,3,4], 2) #== [2,4,4,5,5,6,6,3,3]
+
+######################live coding with Will 2/2
+=begin
+Requirement
+return a string where:
+1) the first and last characters remain in original place for each word
+2) characters between the first and last characters must be sorted alphabetically
+3) punctuation should remain at the same place as it started, for example: shan't -> sahn't
+Assumptions
+1) words are seperated by single spaces
+2) only spaces separate words, special characters do not, for example: tik-tak -> tai-ktk
+3) special characters do not take the position of the non special characters, for example: -dcba -> -dbca
+4) for this kata puctuation is limited to 4 characters: hyphen(-), apostrophe('), comma(,) and period(.)
+5) ignore capitalisation
+for reference: http://en.wikipedia.org/wiki/Typoglycemia
+  =end
+  =begin
+  PROBLEM
+  input: a string with at least one word
+  output: a string with the word(s) changed according to a bunch of rules
+
+  explicit rules:
+  words are sep by spaces, not punctuation
+  first and last letter of each word stay the same
+  other letters inside the word sorted by alpha
+  punctuation stays the same location
+  special characters do not take the place of first or last letter
+
+  implicit rules: ok
+
+  Questions? ok
+
+  mental model:
+  split into an Array
+  each with index
+  hash for punctuation and indeces?
+
+  EXAMPLES ok
+
+  DATA / ALGORITHM
+  # arrays - one for the whole String, one for each word
+
+helper method to handle letters: take string, keep first and last, sort the
+middle letters
+
+  # for each punctuation in the word, store the index as key, char as value
+  init first to array[0]
+  init last to array [-1]
+
+  iterate with EWI
+  for each word in the array that reps the string of words
+    #map!, for each character in each word
+      if the index is equal to any key values, (character is a punctuation), store the value for that hash key
+      if the character is middle chars, replace with the next consecutive char from the sorted character array
+      if the middle-subarray is empty, shovel in the last character
+      end
+      join the returned Array
+      join the whole array
+  CODE
+=end
+
+def scramble_singular(str)
+  hash = {}
+  str.chars.each_with_index do |c,i|
+    if c =~ /[^\w]/
+      hash[i] = c
+    end
+  end
+  puts hash
+end
+scramble_singular("-hey-that's")
+
+def scramble_words(string)
+  words = string.split
+  words.each do |word|
+
+  end
+end
+
+p "scramble_words"
+p scramble_words('professionals') == 'paefilnoorsss'
+p scramble_words('i') == 'i'
+p scramble_words('') == ''
+p scramble_words('me') == 'me'
+p scramble_words('you') == 'you'
+p scramble_words('card-carrying') == 'caac-dinrrryg'
+p scramble_words("shan't") == "sahn't"
+p scramble_words('-dcba') == '-dbca'
+p scramble_words('dcba.') == 'dbca.'
+p scramble_words("you've gotta dance like there's nobody watching, love like you'll never be hurt, sing like there's
+  nobody listening, and live like it's heaven on earth.") == "you've gotta dacne like teehr's nbdooy wachintg, love like ylo'ul
+  neevr be hrut, sing like teehr's nbdooy leiinnstg, and live like it's haeevn on earth."
+
+
+=begin
+
+Alphabet symmetry
+
+Consider the word "abode". We can see that the letter a is in position 1
+and b is in position 2. In the alphabet, a and b are also in positions 1 and 2.
+Notice also that d and e in abode occupy the positions they would occupy in
+the alphabet, which are positions 4 and 5.
+
+
+Given an array of words, return an array of the number of letters that occupy
+ their positions in the alphabet for each word. For example,
+
+
+solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+
+See test cases for more examples.
+
+
+Input will consist of alphabet characters, both uppercase and lowercase. No
+spaces.
+
+
+Good luck!
+
+
+If you like this Kata, please try:
+
+
+Last digit symmetry
+
+
+Alternate capitalization
+
+
+=end
+def solve(array)
+
+end
+p solve(["abode","ABc","xyzD"]) == [4,3,1]
+
+p solve(["abide","ABc","xyz"]) == [4,3,0]
+
+p solve(["IAMDEFANDJKL","thedefgh","xyzDEFghijabc"])== [6,5,7]
+
+p solve(["encode","abc","xyzD","ABmD"]) == [1, 3, 1, 3]
+
+
+
+=begin
+Coding with Caleb 2023/1/31
+You've just discovered a square (NxN) field and you notice a warning sign. The sign states that there's a treasure chest in the 2D grid-like field in front of you.
+
+Write a function mineLocation/MineLocation that accepts a 2D array, and returns the location of the mine. The mine is represented as the integer 1 in the 2D array. Areas in the 2D array that are not the mine will be represented as 0s.
+
+The location returned should be an array (Tuple<int, int> in C#) where the first element is the row index, and the second element is the column index of the bomb location (both should be 0 based). All 2D arrays passed into your function will be square (NxN), and there will only be one mine in the array.
+
+PROBLEM
+
+input: a 2d array that represents a feild with a treasure chest
+output: [a,b] where a is the row index and b is the column index of TC
+
+explicit rules: one TC
+all square array input
+return the location row x column
+
+implicit rules: ok
+
+Questions: ok
+
+Mental Model:
+each w index seems useful
+iterate to find the subarray that includes the chest
+iterate through that subarray to find the element index location of the chest
+store each value in the location array, return the location array
+
+EXAMPLES
+
+DATA / ALGORITHM
+arrays
+
+init location_array
+iterate through original array and check which subarray includes a 1
+store the index value of that subarray in the location Array
+  iterate through the sub array to find the index value of the element 1
+  store that value in the location array, return the location Array
+
+
+for each subarray in array:
+  check if the subarray includes 1, if so, store that index value in the location_array
+  within that Array
+    find the index of the element 1, store that
+  return the location_array
+
+
+CODE
+=end
+
+def mineLocation(array)
+  location_array = []
+  array.each_with_index do |sub, index|
+    if sub.include?(1)
+      location_array << index
+      location_array << sub.index(1)
+      return location_array
+    end
+  end
+end
+
+# 18 min
+# keep up the talk then type; makes it better communication
+# try the algorithm style of starting each explanation of an iteration with the phrase "for each ___ in ____" and then indent to describe what happens on each iteration.
+
+
+p mineLocation( [ [1, 0, 0], [0, 0, 0], [0, 0, 0] ] ) == [0, 0]
+p mineLocation( [ [0, 0, 0], [0, 1, 0], [0, 0, 0] ] ) == [1, 1]
+p mineLocation( [ [0, 0, 0], [0, 0, 0], [0, 1, 0] ] ) == [2, 1]
+p mineLocation([ [1, 0], [0, 0] ]) == [0, 0]
+p mineLocation([ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0] ]) == [2, 2]
+
+# From Chris Shieh 1/31/2023
+# if I finish this, can I write a version that checks for down then up
+# have one method that check for both conditions, returns true if either up
+# then down, or down then up is true a spiral array is an array that goes up
+# in value and down, write a method that tests an array to see if it is a spiral
+
+=begin
+PROBLEM
+input: array of integers
+output: boolean
+
+explicit rules: if each consecutive number, up the max, is increasing, and
+then each number after the max decreases, output true, otherwise output false
+
+implicit rules: numbers can have gaps between them of different sizes
+nums are non-negative
+
+Questions?
+
+MM: each with a return statement for returning false
+double boolean, on left of max, less than max and greater than previous,
+inverse other side of max
+
+
+EXAMPLES
+
+DATA / ALGORITHM
+# init max variable, store the max of the array
+# check if max occurs twice, if so, return false
+# init max index variable and store index value
+for each value in the array,
+  if index is less than max index and value is greater than previous element
+    next
+  elsif index is greater than max index and value is less than previous
+    next
+  else
+    return false
+end
+true
+
+
+CODE
+=end
+def spiral(array)
+  max = array.max
+  return false if array.count(max) > 1
+  max_index = array.index(max)
+  array.each_with_index do |n, i|
+    if i < max_index && n < array[i+1]
+      next
+    elsif i > max_index && n > array[i+1]
+      next
+    elsif i == (array.size - 1)
+      return true
+    else
+      return false
+    end
+  end
+end
+
+p "-------sprial tests"
+p spiral([1,2,3,2,1]) #== true
+p spiral([1,2,4,6,4,2,1]) == true
+p spiral([0,3,5,4,3,2,1]) == true
+p spiral([0,3,5,5,4,3,2,1]) == false
+p spiral([1,2,4,6,4,2,4]) == false
+
+
+
+# verbalize expectation of a test before running the test
+
+
+
+
+
+
+=begin
+6 kyu
+
+Given: an array containing hashes of names
+
+
+Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+
+
+  Example:
+
+
+  list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+
+# returns 'Bart, Lisa & Maggie'
+
+
+list([ {name: 'Bart'}, {name: 'Lisa'} ])
+
+# returns 'Bart & Lisa'
+
+
+list([ {name: 'Bart'} ])
+
+# returns 'Bart'
+
+
+list([])
+
+# returns ''
+
+Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
+
+=end
