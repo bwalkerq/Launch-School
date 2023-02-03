a2b78895d89fac2add689e2a72057ca653513c09
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-02-03 13:53:19 -0600

    12. panagram done

--- RB101/Assessments/Interview Practice/Codewars problems.rb
+++ RB101/Assessments/Interview Practice/Codewars problems.rb
@@ -399,45 +399,50 @@ p spiral([1,2,4,6,4,2,1]) == true
 p spiral([0,3,5,4,3,2,1]) == true
 p spiral([0,3,5,5,4,3,2,1]) == false
 p spiral([1,2,4,6,4,2,4]) == false
-
-
-
 # verbalize expectation of a test before running the test
 
+=begin
+12. Detect Pangram
+(https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/ruby)
+6 kyu
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The
+quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+  Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+=end
+def panagram?(string)
+  ('a'..'z').to_a.each do |letter|
+    if string.downcase.include?(letter)
+      next
+    else
+      return false
+    end
+  end
+  true
+end
 
-
-
+p "panagram? tests"
+p panagram?("The quick brown fox jumps over the lazy dog.") == true
+p panagram?("This is not a pangram.") == false
 
 
 =begin
 6 kyu
 
 Given: an array containing hashes of names
-
-
-Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
-
+Return: a string formatted as a list of names separated by commas except for
+the last two names, which should be separated by an ampersand.
 
   Example:
-
-
   list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
-
 # returns 'Bart, Lisa & Maggie'
 
-
 list([ {name: 'Bart'}, {name: 'Lisa'} ])
-
 # returns 'Bart & Lisa'
 
-
 list([ {name: 'Bart'} ])
-
 # returns 'Bart'
 
-
 list([])
-
 # returns ''
 
 Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
