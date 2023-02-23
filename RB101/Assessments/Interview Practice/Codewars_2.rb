6b48c799fe5311f353813629e5408876abbb5b7f
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-02-23 15:39:47 -0600

    Changes by jon

--- RB101/Assessments/Interview Practice/Codewars_2.rb
+++ RB101/Assessments/Interview Practice/Codewars_2.rb
@@ -1335,8 +1335,21 @@ end
 # p missing_letter(["O","Q","R","S"]) #== "P"
 
 
+
 =begin
-PROBLEM
+Complete the function that accepts two integer arrays of equal length and
+-compares the value of each member in one array to the corresponding member in
+the other
+-squares the absolute value difference between those two values
+-and returns the average of those squared absolute value difference between
+each member pair.
+Examples
+[1, 2, 3], [4, 5, 6]              -->   9   because (9 + 9 + 9) / 3
+[10, 20, 10, 2], [10, 25, 5, -2]  -->  16.5 because (0 + 25 + 25 + 16) / 4
+[-1, 0], [0, -1]                  -->   1   because (1 + 1) / 2
+
+PROBLEM given two arrays of equal length do some math with corresponding values
+and return the average
 
 input: two arrays of integers
 output: an intger
