9ed1e93f5ca4e27689b94d4bc9d3b742b7c336cf
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-03-13 10:57:24 -0400

    updating coaching with two problems from Mai K

--- RB 120/RB 119 interview coaching.rb
+++ RB 120/RB 119 interview coaching.rb
@@ -1,3 +1,11 @@
+# Feedback that i often give:
+# Think of the interview as a presentation of your process; you want to make
+# it transparent what you're thinking and why, and also package it in a way
+# that is easy for them to understand and follow. Speak then type, avoid
+# doing at the same time.
+
+
+PROBLEMS
 # # Write a function that connects each previous word to the next word by the shared letters. Return the resulting string (removing duplicate characters in the overlap) and the minimum number of shared letters across all pairs of strings.
 
 p join(["oven", "envier", "erase", "serious"])  == ["ovenvieraserious", 2]
@@ -25,14 +33,58 @@ p spiral([0,3,5,5,4,3,2,1]) == false
 p spiral([1,2,4,6,4,2,4]) == false
 p spiral([1,5,4,6,4,2,1]) == false
 
-# feedback for Esther Kim
-#
+=begin
+Problem from Mai Khu
+
+Task :
+Given a List [] of n integers , find the minimum number to be inserted in a list, so that the sum of all elements of the list should equal the closest prime number.
+
+Notes
+List size is at least 2 .
+
+List's numbers will only have positives (n > 0) .
+
+Repetition of numbers in the list could occur .
+
+The newer list's sum should equal the closest prime number .
+
+Input >> Output Examples
+1- minimumNumber ({3,1,2}) ==> return (1)
+Explanation:
+Since , the sum of the list's elements equal to (6) , the minimum number to be inserted to transform the sum to prime number is (1) , which will make *the sum of the List** equal the closest prime number (7)* .
+2-  minimumNumber ({2,12,8,4,6}) ==> return (5)
+Explanation:
+Since , the sum of the list's elements equal to (32) , the minimum number to be inserted to transform the sum to prime number is (5) , which will make *the sum of the List** equal the closest prime number (37)* .
+3- minimumNumber ({50,39,49,6,17,28}) ==> return (2)
+Explanation:
+Since , the sum of the list's elements equal to (189) , the minimum number to be inserted to transform the sum to prime number is (2) , which will make *the sum of the List** equal the closest prime number (191)* .
+=end
+
+p minimum_number([3,1,2]) == 1
+p minimum_number([5,2]) == 0
+p minimum_number([1,1,1]) == 0
+p minimum_number([2,12,8,4,6]) == 5
+p minimum_number([50,39,49,6,17,28]) == 2
 
 
-# Feedback that i often give:
-# Think of the interview as a presentation of your process; you want to make
-# it transparent what you're thinking and why, and also package it in a way
-# that is easy for them to understand and follow. Speak then type, avoid
-# doing at the same time.
 
+=begin
+Write a function
 
+triple_double(num1, num2)
+which takes numbers num1 and num2 and returns 1 if there is a straight triple of a number at any place in num1 and also a straight double of the same number in num2.
+
+If this isn't the case, return 0
+
+Examples
+triple_double(451999277, 41177722899) == 1
+# num1 has straight triple 999s and  num2 has straight double 99s
+
+triple_double(1222345, 12345) == 0
+# num1 has straight triple 2s but num2 has only a single 2
+=end
+
+p triple_double(66677789, 12345667) == 1
+p triple_double(66666666789, 12345667) == 1
+p triple_double(666789, 12345677) == 0
+p triple_double("asfaaas", "aakjdfgsdfg") == 1
