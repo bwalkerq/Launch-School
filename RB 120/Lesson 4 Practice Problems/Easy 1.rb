0396302d053f8f8664f23509bb0a5f0737867aff
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-04-28 15:56:06 -0500

    RB 120 L4 easy 1 and 2 complete

---
+++ RB 120/Lesson 4 Practice Problems/Easy 1.rb
@@ -0,0 +1,20 @@
+# 1. all of these are objects, and we can call #class on them to determine
+# that their classes are TrueClass (not Boolean), String, Array, Integer,
+# respectively
+
+# 2 - remember to instantiate objects to call instance methods
+# 3-the end, done. Easy to figure out in head, and moved on.
+
+# All these problems are easy so far
+# Because there are so many benefits to using OOP we will just summarize some of the major ones:
+#
+# Creating objects allows programmers to think more abstractly about the code they are writing.
+# Objects are represented by nouns so are easier to conceptualize.
+# It allows us to only expose functionality to the parts of code that need it, meaning namespace issues are much harder to come across.
+# It allows us to easily give functionality to different parts of an application without duplication.
+# We can build applications faster as we can reuse pre-written code.
+# As the software becomes more complex this complexity can be more easily managed.
+
+# Easy 3
+# solid, these went fast
+
