a074a609e11937de46d192780a47e7880ec2fa32
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-05-03 10:49:36 -0500

    OOP TicTacToe improvements 1-6

--- RB 120/Projects (games)/oop_tic_tac_toe.rb
+++ RB 120/Projects (games)/oop_tic_tac_toe.rb
@@ -25,7 +25,7 @@ class Board
   end
 
   def someone_won?
-    !!detect_winner
+    !!winning_marker
   end
 
   def count_human_marker(squares)
@@ -36,7 +36,7 @@ class Board
     squares.collect(&:marker).count(TTTGame::COMPUTER_MARKER)
   end
 
-  def detect_winner #return winning marker, or return nil
+  def winning_marker #return winning marker, or return nil
     WINNING_LINES.each do |line|
       if count_human_marker(@squares.values_at(*line)) == 3
         return TTTGame::HUMAN_MARKER
@@ -49,6 +49,24 @@ class Board
 
   def reset
     (1..9).each { |key| @squares[key] = Square.new }
+  end
+
+  def draw
+    puts %(
+      -------------------
+      "     |     |     "
+      "  #{get_square_at(1)}  |  #{get_square_at(2)}  |  #{get_square_at(3)}  "
+      "     |     |     "
+      "-----+-----+-----"
+      "     |     |     "
+      "  #{get_square_at(4)}  |  #{get_square_at(5)}  |  #{get_square_at(6)}  "
+      "     |     |     "
+      "-----+-----+-----"
+      "     |     |     "
+      "  #{get_square_at(7)}  |  #{get_square_at(8)}  |  #{get_square_at(9)}  "
+      "     |     |     "
+      "-----+-----+-----"
+      )
   end
 end
 
@@ -98,24 +116,16 @@ class TTTGame
     puts "Thanks for playing, c'mon back now, ya hear?"
   end
 
-  def display_board(clear = true)
-    system 'clear' if clear
+  def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
-    puts %(
-      -------------------
-      "     |     |     "
-      "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}  "
-      "     |     |     "
-      "-----+-----+-----"
-      "     |     |     "
-      "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}  "
-      "     |     |     "
-      "-----+-----+-----"
-      "     |     |     "
-      "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}  "
-      "     |     |     "
-      "-----+-----+-----"
-      )
+    puts ""
+    board.draw
+    puts ""
+  end
+
+  def clear_screen_and_display_board
+    clear
+    display_board
   end
 
   def human_moves
@@ -135,9 +145,9 @@ class TTTGame
   end
 
   def display_result
-    display_board
+    clear_screen_and_display_board
 
-    case board.detect_winner
+    case board.winning_marker
     when human.marker
       puts "You won!"
     when computer.marker
@@ -159,25 +169,43 @@ class TTTGame
     answer == "y"
   end
 
-  def play
+  def clear
     system 'clear'
+  end
+
+  def game_reset
+    board.reset
+    clear
+  end
+
+  def display_play_again_message
+    puts "Let's play again!"
+    puts ""
+  end
+
+
+  def play
+    clear
     display_welcome_message
+
     loop do
-      display_board(false)
+      display_board
+
       loop do
         human_moves
         break if board.someone_won? || board.full?
 
         computer_moves
         break if board.someone_won? || board.full?
-        display_board
+
+        clear_screen_and_display_board
       end
       display_result
       break unless play_again?
-      board.reset
-      system 'clear'
-      puts "Let's play again!"
+      game_reset
+      display_play_again_message
     end
+
     display_goodbye_message
   end
 end
