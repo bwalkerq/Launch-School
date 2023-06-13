8d88a147a585d8bb03af9155c31f4748c11c6242
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-06-13 08:53:24 -0500

    playing with assertion style, testing another instance variable

--- RB130/car.rb
+++ RB130/car.rb
@@ -1,8 +1,9 @@
 
 class Car
-  attr_accessor :wheels
+  attr_accessor :wheels, :doors
 
   def initialize
     @wheels = 4
+    @doors = 2
   end
 end
--- RB130/car_test.rb
+++ RB130/car_test.rb
@@ -8,6 +8,11 @@ require_relative 'car'
 class CarTest < MiniTest::Test
   def test_wheels
     car = Car.new
-    assert_equal(3, car.wheels)
+    assert_equal(4, car.wheels)
+  end
+
+  def test_doors
+    car = Car.new
+    assert_equal(2, car.doors)
   end
 end
