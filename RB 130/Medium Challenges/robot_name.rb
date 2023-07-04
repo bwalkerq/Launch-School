a98b9f20a52aa82b570e792767a199f5bcfbebbf
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-07-04 12:29:29 -0500

    Robot Medium Challenge refactored

--- RB 130/Medium Challenges/robot_name.rb
+++ RB 130/Medium Challenges/robot_name.rb
@@ -1,23 +1,24 @@
 
 class Robot
   attr_reader :name
-  @@previous_names = []
+  @@names = []
 
   def initialize
     @name = random_name
-    @@previous_names << @name
+    @@names << @name
   end
 
   def random_name
     new_name = nil
     loop do
       new_name = (('A'..'Z').to_a.sample(2) + (1..9).to_a.sample(3)).join
-      break unless @@previous_names.include?(new_name)
+      break unless @@names.include?(new_name)
     end
     new_name
   end
 
   def reset
+    @@names.delete(name)
     self.name = random_name
   end
 
