3d944937a898e7c9291ad5c7e01008ce7960e8e6
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-08-31 15:11:26 -0500

    complete dynamic directory index challenge

--- RB 175/dynamic_directory_index/ddi.rb
+++ RB 175/dynamic_directory_index/ddi.rb
@@ -1,1 +1,15 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
+
+get '/' do
+  @file_names = Dir.glob('public/*')
+  @file_names.map! do |string|
+    string.split('/').last
+  end.sort!
+  @file_names.reverse! if (params[:sort] == 'descending')
+
+  erb :home
+end
+
 
