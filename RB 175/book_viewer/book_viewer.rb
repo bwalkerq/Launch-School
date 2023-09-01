3495729170bd8fd15311d6fbec96eb8e406daeb9
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-09-01 10:12:01 -0500

    chapters link, chapter pages display title of the chapter, added the default layout.erb

--- RB 175/book_viewer/book_viewer.rb
+++ RB 175/book_viewer/book_viewer.rb
@@ -2,16 +2,22 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines "data/toc.txt"
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines "data/toc.txt"
+
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines "data/toc.txt"
-  @chapter = File.read "data/chp1.txt"
+get "/chapters/:number" do
+  number = params[:number]
+  chapter_name = @contents[number.to_i - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+
+  @chapter = File.read "data/chp#{number}.txt"
 
   erb :chapter
 end
