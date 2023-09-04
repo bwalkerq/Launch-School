bcd5583cc84d82487cc270ece43534e495645d03
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-09-04 11:11:38 -0500

    First view helper defined, in_paragraphs, to format chapter text into paragraphs.

--- RB 175/book_viewer/book_viewer.rb
+++ RB 175/book_viewer/book_viewer.rb
@@ -6,6 +6,12 @@ before do
   @contents = File.readlines "data/toc.txt"
 end
 
+helpers do
+  def in_paragraphs(string)
+    string.split('\n\n').map{ |paragraph| "<p>#{paragraph}</p>"}.join
+  end
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
 
