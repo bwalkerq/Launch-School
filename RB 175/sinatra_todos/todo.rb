1ce67e9f821bb5aca93f1c78f7f10c37e7870f70
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-09-22 10:11:41 -0500

    error validation logic improved

--- RB 175/sinatra_todos/todo.rb
+++ RB 175/sinatra_todos/todo.rb
@@ -26,16 +26,27 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Return an error mesage if the name is invalid. Return nil if name is valid
+def error_for_list_name(name)
+  if !(1..10).cover? name.size
+    'The list name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'The list name must be unique.'
+  end
+end
+
 # Create a new list
 post "/lists" do
   list_name = params[:list_name].strip
-  if (1..10).cover? list_name.size
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
     session[:lists] << { name: list_name, todos: [] }
     session[:success] = "The list has been created."
     redirect "/lists"
-  else
-    session[:error] = "The list name must be between 1 and 100 characters"
-    erb :new_list, layout: :layout
   end
 end
 
