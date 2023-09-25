e25594a4a5c1163f95feeff0dfdecd03533b2f2c
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-09-25 10:58:30 -0500

    editing and deleting todo lists (up through assignment 17)

--- RB 175/sinatra_todos/todo.rb
+++ RB 175/sinatra_todos/todo.rb
@@ -30,7 +30,7 @@ end
 
 # Return an error mesage if the name is invalid. Return nil if name is valid
 def error_for_list_name(name)
-  if !(1..10).cover? name.size
+  if !(1..100).cover? name.size
     'The list name must be between 1 and 100 characters.'
   elsif session[:lists].any? { |list| list[:name] == name }
     'The list name must be unique.'
@@ -57,3 +57,36 @@ get '/lists/:id' do
   @list = session[:lists][id]
   erb :list, layout: :layout
 end
+
+# Edit an existing todo list
+get '/list/:id/edit' do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  erb :edit_list, layout: :layout
+end
+
+#Update an existing todo list
+post '/lists/:id' do
+  list_name = params[:list_name].strip
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = 'The list has been updated.'
+    redirect "/lists/#{id}"
+  end
+end
+
+#delete a todo list
+post '/lists/:id/destroy' do
+  id = params[:id].to_i
+  session[:lists].delete_at(id)
+  session[:success] = 'the list was deleted.'
+  redirect '/lists'
+end
+
