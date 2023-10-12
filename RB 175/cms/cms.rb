51e80d050747abd6c1aa8c0b2c327bbdf76691b4
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-10-12 09:16:44 -0500

    handles bad paths with an error message

--- RB 175/cms/cms.rb
+++ RB 175/cms/cms.rb
@@ -5,6 +5,12 @@ require 'tilt/erubis'
 
 root = File.expand_path('..', __FILE__)
 
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+  set :erb, :escape_html => true
+end
+
 get '/' do
   @files = Dir.glob(root + '/data/*').map do |path|
     File.basename(path)
@@ -13,9 +19,20 @@ get '/' do
   erb :index
 end
 
-get "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+# add behavior to handle a bad request; if the file doesn't exist
+# redirect to home and flash message
+# the error message is stored in params hash I think
+# the flash message part deletes the error message so on refresh the flash disappears
 
-  headers["Content-Type"] = "text/plain"
-  File.read(file_path)
+get "/:filename" do
+  file_name = params[:filename]
+  file_path = root + "/data/" + file_name
+
+  if File.file?(file_path)
+    headers["Content-Type"] = "text/plain"
+    File.read(file_path)
+  else
+    session[:message] = "#{file_name} does not exist."
+    redirect '/'
+  end
 end
---
+++ RB 175/cms/test/cms_test.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'rack/test'
+
+require_relative '../cms'
+
+class CmsTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get '/'
+
+    assert_equal 200, last_response.status
+    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
+    assert_includes last_response.body, 'about.txt'
+    assert_includes last_response.body, 'changes.txt'
+    assert_includes last_response.body, 'history.txt'
+  end
+
+  def test_viewing_text_document
+    get '/history.txt'
+
+    assert_equal 200, last_response.status
+    assert_equal 'text/plain', last_response['Content-Type']
+    assert_includes last_response.body, 'Ruby 0.95 released'
+  end
+
+  def test_nonexistant_document
+    get '/nofile.txt'
+
+  end
+end
