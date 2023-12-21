a36a3693fdef165412fb491336aaf345baa15a79
Benjamin Walker <benjamin.q.walker@gmail.com>
2023-12-21 14:17:16 -0600

    help doc and result based on arguments passed complete

--- RB 185/expense_project/expense.rb
+++ RB 185/expense_project/expense.rb
@@ -3,15 +3,40 @@
 require 'pg'
 require 'bundler/setup'
 
-connection = PG.connect(dbname: 'expenses')
+def display_help
+  puts <<~HELP
 
-result = connection.exec('SELECT * FROM expenses ORDER BY created_on ASC')
-result.each do |tuple|
-  columns = [tuple['id'].rjust(3),
-             tuple['created_on'].rjust(10),
-             tuple['amount'].rjust(12),
-             tuple['memo']]
-
-  puts columns.join(" | ")
+    An expense recording system
+    
+    Commands:
+    
+    add AMOUNT MEMO - record a new expense
+    clear - delete all expenses
+    list - list all expenses
+    delete NUMBER - remove expense with id NUMBER
+    search QUERY - list expenses with a matching memo field
+  HELP
 end
 
+def list_expenses
+  connection = PG.connect(dbname: 'expenses')
+
+  result = connection.exec('SELECT * FROM expenses ORDER BY created_on ASC')
+  result.each do |tuple|
+    columns = [tuple['id'].rjust(3),
+               tuple['created_on'].rjust(10),
+               tuple['amount'].rjust(12),
+               tuple['memo']]
+
+    puts columns.join(" | ")
+  end
+end
+
+arguments = ARGV
+
+case arguments[0]
+when 'list'
+  list_expenses
+else
+  display_help
+end
