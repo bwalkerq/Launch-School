92458054fda7df2aa9256cf8c94631ac770a713d
Benjamin Walker <benjamin.q.walker@gmail.com>
2024-02-22 08:50:57 -0600

    more JS basics exercises from the first part of the 210 course

--- JS 210/javascript_basics_exercises.js
+++ JS 210/javascript_basics_exercises.js
@@ -76,11 +76,22 @@ if (apples === bananas) {
 let lastName = 'Walker';
 let familyMessage = lastName === 'Walker' ? "You're part of the fam!" : "Nah";
 
-console.log(familyMessage)
+// console.log(familyMessage)
+
+function average(arr) {
+  return sum(arr) / arr.length;
+}
+
+function sum(arr) {
+  let total = 0
+  for (let index = 0; index < arr.length; index++) {
+    total += arr[index]
+  }
+  return total;
+}
 
 
-
-
+console.log(average([30,4,5,5,1]));
 
 
 
