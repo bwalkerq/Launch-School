3267c5bf3770c5df9834a0830a2c88f831bd71db
Benjamin Walker <benjamin.q.walker@gmail.com>
2024-03-05 15:28:37 -0600

    L4 up through Assignment 8 with #arraysEqual custom function

--- JS 210/L4_exercises.js
+++ JS 210/L4_exercises.js
@@ -121,16 +121,86 @@ function slice(arr, startIndex, endIndex) {
 slice([1, 2, 3, 4, 5], 0, 2);                      // [ 1, 2 ]
 slice(['a', 'b', 'c', 'd', 'e', 'f', 'g'], 1, 3);  // [ 'b', 'c' ]
 
+/*
+Write a function named splice that accepts three arguments: an Array, a
+start index, and the number of values to remove. The function should remove
+values from the original Array, starting with the start index and removing
+the specified number of values. The function should return the removed values
+ in a new Array.
 
 
+ */
+function splice(arr, startIndex, numberToRemove) {
+  let splicedArray = []
 
+  for (let index = startIndex; index < arr.length; index++) {
+    if (index < startIndex + numberToRemove) {
+      push(splicedArray, arr[index]);
+    } else if (index >= startIndex + numberToRemove) {
+      arr[index - numberToRemove] = arr[index];
+    }
+  }
+  arr.length = arr.length - numberToRemove;
+  return splicedArray
+}
 
+count = [1, 2, 3, 4, 5, 6, 7, 8];
+// console.log(splice(count, 2, 5)); // == [ 3, 4, 5, 6, 7 ]);
+// console.log(count);                                 // [ 1, 2, 8 ]
 
+function concat(arr1, arr2) {
+  let newArray = [];
+  for (let index = 0; index < arr1.length; index++) {
+    push(newArray, arr1[index]);
+  }
 
+  for (let index = 0; index < arr2.length; index++) {
+    push(newArray, arr2[index]);
+  }
+  return newArray;
+}
 
+// console.log(concat([1, 2, 3], [4, 5, 6]));       // [ 1, 2, 3, 4, 5, 6 ]
 
+function join(array, string) {
+  let joinedString = '';
 
+  for (let index = 0; index < array.length; index++) {
+    joinedString += String(array[index]);
+    if (index < array.length - 1) {
+      joinedString += string;
+    }
+  }
 
+  console.log(joinedString);
+  return joinedString;
+}
+
+// join(['bri', 'tru', 'wha'], 'ck ');       // 'brick truck wha'
+// join([1, 2, 3], ' and ');                 // '1 and 2 and 3'
+
+function arraysEqual(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+
+  for (let index = 0; index < firstArray.length; index++) {
+    if (firstArray[index] !== secondArray[index]) {
+      // console.log(false)
+      return false;
+    }
+  }
+  // console.log(true)
+  return true;
+}
+
+console.log(arraysEqual([1], [1]) === true);
+console.log(arraysEqual([1], [2]) === false);
+console.log(arraysEqual([1, 2], [1, 2, 3]) === false);
+console.log(arraysEqual([1, 'hi', true], [1, 'hi', true]) === true);
+console.log(arraysEqual([1, 'hi', true], [1, 'hi', false]) === false);
+console.log(arraysEqual([1, 'hi', true], [1, 'hello', true]) === false);
+console.log(arraysEqual([1, 'hi', true], [2, 'hi', true]) === false);
 
 
 
