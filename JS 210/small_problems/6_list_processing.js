1ee39c013531eb201dedad4407b6c2ebdf82f9b8
Benjamin Walker <benjamin.q.walker@gmail.com>
2024-04-07 17:50:06 -0500

    list processing progress, Sunday!

--- JS 210/small_problems/6_list_processing.js
+++ JS 210/small_problems/6_list_processing.js
@@ -87,12 +87,48 @@ function sumOfSums(arr) {
   return result;
 }
 
+leadingSubstrings('abc');      // ["a", "ab", "abc"]
+leadingSubstrings('a');        // ["a"]
+leadingSubstrings('xyzzy');    // ["x", "xy", "xyz", "xyzz", "xyzzy"]
 
+function leadingSubstrings(str) {
+  let arr = str.split('');
+  return arr.map((letter, idx) => arr.slice(0, idx + 1).join(''));
+}
 
+function substrings(str) {
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    let sub = leadingSubstrings(str.slice(i))
+    result = result.concat(sub);  // in this line I learned that concat does not
+    // mutate, but returns a new array.
+  }
+  return result
+}
 
+console.log(substrings('abcde'));
+// [ "a", "ab", "abc", "abcd", "abcde",
+//   "b", "bc", "bcd", "bcde",
+//   "c", "cd", "cde",
+//   "d", "de",
+//   "e" ]);
 
+palindromes('abcd');       // []
+palindromes('madam');      // [ "madam", "ada" ]
 
+palindromes('hello-madam-did-madam-goodbye');
+// returns
+[ "ll", "-madam-", "-madam-did-madam-", "madam", "madam-did-madam", "ada",
+  "adam-did-mada", "dam-did-mad", "am-did-ma", "m-did-m", "-did-", "did",
+  "-madam-", "madam", "ada", "oo" ]
 
+palindromes('knitting cassettes');
+// returns
+[ "nittin", "itti", "tt", "ss", "settes", "ette", "tt" ]
+
+function palindromes(str) {
+
+}
 
 
 
