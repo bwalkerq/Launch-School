f3f8661effcd62f4e784ad2d166897ea27c7c05e
Benjamin Walker <benjamin.q.walker@gmail.com>
2024-05-06 14:36:05 -0500

    L2 exercises complete through assignment 4

--- LS 215/L1_exercises.js
+++ LS 215/L1_exercises.js
@@ -408,59 +408,6 @@ function transpose(array) {
 // console.log(scoresByStudent)
 // console.log(transpose(scoresByStudent))
 
-let firstName = 'Viola';
-let lastName = 'McCarthy';
-let fullName = firstName + ' ' + lastName;
-// console.log(fullName)
-
-// console.log(firstName.concat(lastName));
-// console.log(fullName.split(' '));
-
-let language = 'JavaScript';
-let idx = language.indexOf('S');
-// console.log(idx);
-let charCode = language.charCodeAt(idx);
-// console.log(charCode);
-// console.log(String.fromCharCode(charCode))
-// console.log(language.lastIndexOf('a'));
-
-let a = 'a';
-let b = 'b';
-// console.log(a > b);
-b = 'B'
-// console.log(a > b);
-
-// console.log(language.slice(1,4));
-// console.log(language.slice(2,4));
-// console.log(language.substring(1,4));
-// console.log(language.substring(2,4));
-
-// console.log(language.slice(1,-1));
-// console.log(language.slice(2,-1));
-// console.log(language.substring(1,-1)); // same as (0,1) becase neg numbers
-// considered 0, and
-// substring always interprets the smaller number as the start, and the second
-// as the length.
-// console.log(language.substring(2,-1)); // same as (0,2)
-
-let fact1 = 'JavaScript is fun';
-let fact2 = 'Kids like it too';
-let compoundSentence = fact1 + ' and ' + fact2.toLowerCase();
-// console.log(compoundSentence)
-
-let pi = 22/7;
-// console.log(pi.toString().lastIndexOf('14'))
-
-let boxNumber = (356).toString();
-// console.log(boxNumber);
-
-boxNumber = parseInt(boxNumber);
-// console.log(typeof boxNumber);
-boxNumber = String(boxNumber);
-// console.log(typeof boxNumber);
-
-let rlsync = Re
-
 
 
 
