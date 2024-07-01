f148d255d5084c8d6468ddb3f6f04c626b7f7a40
Benjamin Walker <benjamin.q.walker@gmail.com>
2024-07-01 09:57:01 -0500

    A16  complete

--- JS 220 Object Oriented JS/L5_exercises.js
+++ JS 220 Object Oriented JS/L5_exercises.js
@@ -776,14 +776,49 @@ shelter.adopt(bholmes, kennedy);
 shelter.adopt(bholmes, sweetie);
 shelter.adopt(bholmes, molly);
 shelter.adopt(bholmes, chester);
-shelter.printAdoptions();
-console.log(`${phanson.name} has ${phanson.numberOfPets()} adopted pets.`);
-console.log(`${bholmes.name} has ${bholmes.numberOfPets()} adopted pets.`);
+// shelter.printAdoptions();
+// console.log(`${phanson.name} has ${phanson.numberOfPets()} adopted pets.`);
+// console.log(`${bholmes.name} has ${bholmes.numberOfPets()} adopted pets.`);
 
 
+class Banner {
+  constructor(message) {
+    this.message = message;
+    this.length = message.length;
+  }
 
+  displayBanner() {
+    console.log([this.horizontalRule(), this.emptyLine(), this.messageLine(), this.emptyLine(), this.horizontalRule()].join("\n"));
+  }
 
+  horizontalRule() {
+    return `+-${'-'.repeat(this.length)}-+`
+  }
 
+  emptyLine() {
+    return `| ${' '.repeat(this.length)} |`
+  }
+
+  messageLine() {
+    return `| ${this.message} |`
+  }
+}
+
+let banner2 = new Banner('');
+banner2.displayBanner();
+// +--+
+// |  |
+// |  |
+// |  |
+// +--+
+
+let banner1 = new Banner('To boldly go where no one has gone before.');
+banner1.displayBanner();
+// +--------------------------------------------+
+// |                                            |
+// | To boldly go where no one has gone before. |
+// |                                            |
+// +--------------------------------------------+
 
 
 
