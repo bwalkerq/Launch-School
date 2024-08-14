913fcada315fd71e861952cdb86f347be7a8da98
Benjamin Walker <benjamin.q.walker@gmail.com>
2024-08-14 11:12:55 -0500

    Promise API problems complete

--- JS230/L2_exercises.js
+++ JS230/L2_exercises.js
@@ -380,28 +380,125 @@ function mockAsyncOp() {
 //   .catch(console.error)
 //   .finally(() => console.log('operation attempted'))
 
-function loadData() {
-  return new Promise((resolve, reject) => {
-    if (Math.random() < .5) {
-      resolve('data loaded');
-    } else {
-      reject('network error');
-    }
-  }).catch(() => {
-    console.error('An error occurred. Attempting to recover');
-    return Promise.resolve('Using cached data');
-  });
-}
+// function loadData() {
+//   return new Promise((resolve, reject) => {
+//     if (Math.random() < .5) {
+//       resolve('data loaded');
+//     } else {
+//       reject('network error');
+//     }
+//   }).catch(() => {
+//     console.error('An error occurred. Attempting to recover');
+//     return Promise.resolve('Using cached data');
+//   });
+// }
 
-loadData().then(console.log)
+// loadData().then(console.log)
 /*
 When I solved this, I didn't build the catch into the function, but rather,
 implemented the catch when I called the loadData function. However, I see that
 it makes sense to include it in the function definition. Is this standard
 procedure?
-
-
  */
+
+function flakyService() {
+  return new Promise((resolve, reject) => {
+    if (Math.random() > 0.5) {
+      resolve("Operation successful");
+    } else {
+      reject("Operation failed here");
+    }
+  });
+}
+
+function loadData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() > 0.1) {
+        resolve("Data loaded");
+      } else {
+        reject("Network error");
+      }
+    }, 1000);
+  });
+}
+
+let promises = [flakyService(), flakyService(), loadData()];
+
+// Promise.all(promises).then(console.log).catch(() => console.error('one more more failed'))
+
+
+const firstResource = new Promise((resolve) =>
+  setTimeout(() => resolve("First resource loaded"), 500)
+);
+const secondResource = new Promise((resolve) =>
+  setTimeout(() => resolve("Second resource loaded"), 1000)
+);
+
+// Promise.race([firstResource, secondResource]).then(console.log)
+
+const services = [flakyService(), flakyService(), flakyService()];
+
+// Promise.allSettled(services).then(results => {
+//   results.forEach(result => console.log(result.status))
+// })
+
+// Promise.any(services)
+//   .then(value => console.log(value))
+//   .catch(error => console.error(error))
+
+function loadResource(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .catch(() => 'Failed to Fetch');
+}
+
+// loadResource("https://jsonplaceholder.typicode.com/todos/1").then(console.log);
+// Success response
+
+// loadResource("badUrl.xyz").then(console.log);
+// Failed to fetch
+
+
+/*
+Implement a helper function loadMultipleResources that takes an array of URLs
+ and fetches them using the fetch API. Use Promise.allSettled() to return an
+ array of fetched responses, regardless of whether some URLs may lead to failure.
+ */
+function loadMultipleResources(arr) {
+  let fetched = arr.map(loadResource)
+  return Promise.allSettled(fetched)
+}
+
+// question for Jon, why is this thing returning what it is returning.
+loadMultipleResources([
+  "https://jsonplaceholder.typicode.com/todos/1",
+  "invalidUrl",
+]).then((results) => {
+  results.forEach((result) => {
+    if (result.status === "fulfilled") {
+      console.log("Fetched data:", result.value);
+    } else {
+      console.error(result.reason);
+    }
+  });
+});
+
+// Fetched data: {userId: 1, id: 1, title: 'delectus aut autem', completed: false }
+// Fetched data: Failed to fetch
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
