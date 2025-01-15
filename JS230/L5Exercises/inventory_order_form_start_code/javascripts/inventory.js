f64304d27427b9afd47f33c49314e1a4494d3838
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-01-15 10:29:34 -0600

    L5A8 - finished, and guess a word started

--- JS230/L5Exercises/inventory_order_form_start_code/javascripts/inventory.js
+++ JS230/L5Exercises/inventory_order_form_start_code/javascripts/inventory.js
@@ -1,20 +1,26 @@
-var inventory;
+let inventory;
 
 (function() {
+  let itemTemplate;
+
   inventory = {
     lastId: 0,
     collection: [],
     setDate: function() {
-      var date = new Date();
-      $("#order_date").text(date.toUTCString());
+      let date = new Date();
+      // $("#order_date").text(date.toUTCString());
+      // jquery grabbing an element and setting its text. instead use querySelector and `textContent =`
+      document.querySelector("#order_date").textContent = (date.toUTCString());
     },
     cacheTemplate: function() {
-      var $iTmpl = $("#inventory_item").remove();
-      this.template = $iTmpl.html();
+      itemTemplate = Handlebars.compile(document.querySelector('#inventory_item').innerHTML);
+      /* I didn't save the previous code, but this stores the HB template in the
+      * itemTemplate variable so that object may be passed to it.*/
     },
+
     add: function() {
       this.lastId++;
-      var item = {
+      let item = {
         id: this.lastId,
         name: "",
         stock_number: "",
@@ -30,7 +36,7 @@ var inventory;
       });
     },
     get: function(id) {
-      var found_item;
+      let found_item;
 
       this.collection.forEach(function(item) {
         if (item.id === id) {
@@ -41,43 +47,49 @@ var inventory;
 
       return found_item;
     },
-    update: function($item) {
-      var id = this.findID($item),
+    update: function(previousItem) {
+      let id = this.findID(previousItem),
           item = this.get(id);
 
-      item.name = $item.find("[name^=item_name]").val();
-      item.stock_number = $item.find("[name^=item_stock_number]").val();
-      item.quantity = $item.find("[name^=item_quantity]").val();
+      item.name = previousItem.find("[name^=item_name]").val();
+      item.stock_number = previousItem.find("[name^=item_stock_number]").val();
+      item.quantity = previousItem.find("[name^=item_quantity]").val();
     },
     newItem: function(e) {
       e.preventDefault();
-      var item = this.add(),
-          $item = $(this.template.replace(/ID/g, item.id));
-
-      $("#inventory").append($item);
+      let item = itemTemplate(this.add());
+          // $item = $(this.template.replace(/ID/g, item.id));
+      // $("#inventory").append($item);
+      document.querySelector('#inventory').insertAdjacentHTML("beforeend", item);
     },
     findParent: function(e) {
-      return $(e.target).closest("tr");
+      return e.target.closest("tr");
     },
-    findID: function($item) {
-      return +$item.find("input[type=hidden]").val();
+    findID: function(item) {
+      return +item.querySelector("input[type=hidden]").value;
     },
     deleteItem: function(e) {
       e.preventDefault();
-      var $item = this.findParent(e).remove();
-
-      this.remove(this.findID($item));
+      if (e.target.classList.contains('delete')) {
+      let item = this.findParent(e);
+      this.remove(this.findID(item));
+      item.remove();
+      }
     },
     updateItem: function(e) {
-      var $item = this.findParent(e);
+      let item = this.findParent(e);
 
-      this.update($item);
+      this.update(item);
     },
     bindEvents: function() {
-      $("#add_item").on("click", $.proxy(this.newItem, this));
-      $("#inventory").on("click", "a.delete", $.proxy(this.deleteItem, this));
-      $("#inventory").on("blur", ":input", $.proxy(this.updateItem, this));
+      // $("#add_item").on("click", $.proxy(this.newItem, this));
+      document.querySelector("#add_item").addEventListener("click", this.newItem.bind(this));
+      document.querySelector("#inventory")
+        .addEventListener('click', this.deleteItem.bind(this));
+      document.querySelector("#inventory")
+        .addEventListener('focusout', this.updateItem.bind(this));
     },
+    /**/
     init: function() {
       this.setDate();
       this.cacheTemplate();
@@ -86,4 +98,5 @@ var inventory;
   };
 })();
 
-$($.proxy(inventory.init, inventory));
+// $($.proxy(inventory.init, inventory));
+document.addEventListener('DOMContentLoaded', e => inventory.init.bind(inventory)());
