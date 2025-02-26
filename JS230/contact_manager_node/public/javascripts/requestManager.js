22dbb1b95efbca3599bff9638988c5cb3f251d3c
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-02-26 10:39:20 -0600

    initial refactor for the request manager file

---
+++ JS230/contact_manager_node/public/javascripts/requestManager.js
@@ -0,0 +1,61 @@
+class RequestManager {
+  async fetchContacts() {
+    const response = await fetch('/api/contacts', {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    let contactList = await response.json();
+    contactList.forEach(contact => {
+      contact.tags = contact.tags ? contact.tags.split(',') : [];
+    });
+    return contactList;
+  }
+
+  async addContact(contactObject) {
+    let response = await fetch('/api/contacts' , {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(contactObject)
+    });
+
+    if (response.status >= 400 && response.status <= 499) {
+      const text = await response.text();
+      throw new Error(`Error adding contact" ${text}`);
+    }
+  }
+
+  async updateContact(contactObject) {
+    const id = contactObject.id
+    let response = await fetch(`/api/contacts/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(contactObject)
+    });
+
+    if (response.status >= 400 && response.status <= 499) {
+      const text = await response.text();
+      throw new Error(`Error updating contact" ${text}`);
+    }
+
+    response = await response.json()
+    return response
+  }
+
+  async deleteContact(id) {
+    const response = await fetch(`/api/contacts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.status === 400) {
+      alert('Contact not found');
+    } else {
+      const text = await response.text();
+      throw new Error(`Error deleting contact" ${text}`);
+    }
+  }
+}
