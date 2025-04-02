3394f115c539679f0306b0f81737cabf51ae534b
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-04-02 10:44:45 -0500

    method to create the alphabetized tag set complete

--- JS230/contact_manager_node/public/javascripts/app.js
+++ JS230/contact_manager_node/public/javascripts/app.js
@@ -7,6 +7,7 @@ class Application {
     this.api = new APIRequestManager();
     this.interface = new UserInterface(this);
     this.tags = new Set();
+    this.createTagsSet();
     this.fetchAndRenderContacts();
   }
 
@@ -14,12 +15,26 @@ class Application {
     this.api.fetchContacts().then((response) => {
       this.interface.renderContacts(response)
     });
-    console.log('success')
+  }
+
+  // intended to be called by the UI, returns the tag set
+  async createTagsSet() {
+    let contactsArray = await this.api.fetchContacts();
+    let tagsArray = [];
+    for (const contactsArrayElement of contactsArray) {
+      for (const tag of contactsArrayElement.tags) {tagsArray.push(tag);}
+    }
+    tagsArray.sort();
+
+    for (const tagsArrayElement of tagsArray) {
+      this.tags.add(tagsArrayElement);
+    }
+    return this.tags;
   }
 
   async addContact(contactObject) {
     let response = await this.api.addContact(contactObject);
-    // this.interface.resetAddContactForm()
+    this.interface.resetContactInfoForm() // does it make sense to call this here, or in the UI file?
     this.fetchAndRenderContacts();
   }
 
