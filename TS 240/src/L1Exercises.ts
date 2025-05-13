a74230b7951d96b1e79440e2749123e7077590e2
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-05-13 10:46:56 -0500

    L2 A12 exercises complete

--- TS 240/src/L1Exercises.ts
+++ TS 240/src/L1Exercises.ts
@@ -132,8 +132,21 @@ function displayUserInfo(userInfo: UserInfo): string {
 
 // console.log(displayUserInfo({name: 'benjamin', age: 30}))
 
+interface Point {
+    readonly x: number;
+    readonly y: number;
+}
 
+function movePoint(point: Point, dx: number, dy: number): Point {
+    return {
+        x: point.x + dx,
+        y: point.y + dy,
+    };
+}
 
+// console.log(
+//     movePoint({x: 3, y: 4}, 2, 2)
+// )
 
 
 
