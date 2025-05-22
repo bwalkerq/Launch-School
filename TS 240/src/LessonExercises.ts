3e6aea2beaeed940f98a3c9b70a7a2719da3e43e
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-05-21 21:41:57 -0500

    L4 A18 exercises complete

--- TS 240/src/LessonExercises.ts
+++ TS 240/src/LessonExercises.ts
@@ -383,7 +383,26 @@ function safeGet<T>(array: T[], index: number){
 // console.log(thirdName, number)
 
 
+// A18
 
+function isString(input:unknown): input is string {
+    return (typeof input === 'string')
+}
+
+function processData(data: unknown): string {
+    if (isString(data)) {
+        return `Hello, ${data}`;
+    } else if (typeof data === 'number') {
+        return `Age: ${data}`
+    } else {
+        return 'invalid data'
+    }
+}
+
+// Usage
+console.log(processData("Alice")); // Should print: "Hello, Alice"
+console.log(processData(25)); // Should print: "Age: 25"
+console.log(processData(true)); // Should throw an error: "Invalid data"
 
 
 
