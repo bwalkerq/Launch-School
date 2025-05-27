6a474504e5ae34c2a4b82c68e4e3f333b6d096fc
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-05-26 20:28:01 -0500

    L6 A10 exercises complete

--- TS 240/src/LessonExercises.ts
+++ TS 240/src/LessonExercises.ts
@@ -590,8 +590,59 @@ const formattedNameAgain = formatName({
     title: "Dr.",
 });
 
-console.log(formattedNameAgain); // "Dr. Jane Smith"
-console.log(formatNameAgain({})); // "John Doe"
+// console.log(formattedNameAgain); // "Dr. Jane Smith"
+// console.log(formatNameAgain({})); // "John Doe"
+
+
+
+// A10
+function sqrt(x: number): number {
+    if (x < 0) {
+        throw new Error("Cannot calculate square root of a negative number");
+    }
+    return Math.sqrt(x);
+}
+
+function safeSqrt(x: number): number {
+    try {
+        return sqrt(x)
+    } catch (e: unknown) {
+        if (e instanceof Error) {
+            return -1;
+        } else {
+            throw e;
+        }
+    }
+}
+
+console.log(
+    [100, -7,].map(safeSqrt)
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
