cbecb438cdef7c8a27dabc22f2593f3bddeb515d
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-06-11 13:45:42 -0500

    // Practice: first Binary problem

--- LS 250/DSABook1.ts
+++ LS 250/DSABook1.ts
@@ -356,9 +356,74 @@ function compressToDistinct(nums: number[]): number {
 }
 
 
+// Binary Search Template
+
+// function binaryTemplate(array: number[], target: number) {
+//     let left = 0;
+//     let right = array.length - 1;
+//
+//     while (left <= right) {
+//         let mid = Math.floor((left + right) / 2);
+//
+//         if (array[mid] === target) {
+//             // Optional early return
+//         } else if (***comparison***) {
+//             left = mid + 1;
+//         } else {
+//             right = mid - 1;
+//         }
+//     }
+//
+// // Most often, if the target is not found, additional handling
+// // or returning a specific value is needed. In many cases it will
+// // be the index that `left` variable holds, which would indicate
+// // where the target *would* fit into the array.
+// }
 
 
+// Practice: Find Zero Position
+// Write a function named `findZeroPosition` that takes in a
+// sorted array of distinct integers as input.
+// The function should return the index where the value 0 is
+// found in the array, or the index where it would be inserted if
+// it were not found.
 
+// If the value 0 is found in the array, the function should
+// return the index of the value 0. If the value 0 is not found,
+// the function should return the index where it would be inserted
+// while maintaining the sorted order of the array.
+
+// Example:
+// Input: nums = [-7, -5, -3, 0, 2]
+// Output: 3
+
+console.log(findZeroPosition([-7, -5, -3, 0, 2]) === 3)
+
+// Example:
+// Input: nums = [3, 5, 7, 9, 11]
+// Output: 0
+console.log(findZeroPosition([3, 5, 7, 9, 11]) === 0)
+
+function findZeroPosition(array: number[]): number {
+    let left = 0;
+    let right = array.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (array[mid] === 0) {
+            return mid;
+        } else if (array[mid] < 0) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
+
+
+//
 
 
 
