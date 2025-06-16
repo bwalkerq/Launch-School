b42a2ac0ebc08869a0aa1a9d012fcc76f4e50daa
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-06-16 09:31:57 -0500

    Queue intro complete

--- LS 250/DSA1Continued.js
+++ LS 250/DSA1Continued.js
@@ -190,6 +190,67 @@ const myStack = new Stack();
 // console.log('Peek on empty stack:', myStack.peek());    // logs 'Peek on empty stack: null'
 
 
+// class ListNode {
+//   constructor(val = 0, next = null) {
+//     this.val = val;
+//     this.next = next;
+//   }
+// }
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+  }
+  peek() {
+    // Returns the value of the top most element without removing it.
+    return this.front ? this.front.val : null;
+    // If the queue is empty, it returns `null`.
+  }
+
+  enqueue(value) {
+    // Adds an item to the queue
+    const newNode = new ListNode(value);
+    if (!this.front) {
+    this.back = this.front = newNode;
+    } else {
+      this.back.next = newNode;
+      this.back = newNode
+    }
+  }
+
+  dequeue() {
+    // Removes the item from the queue and returns it
+    if (!this.front) return null;
+
+    let removedNode = this.front;
+    // if the element being dequeued is the last element, change last to null, too
+    if (this.front === this.back) {
+      this.front = null;
+      this.back = null;
+    } else {
+      this.front = this.front.next;
+    }
+    return removedNode.val;
+
+    // If the queue is empty, it returns `null`.
+  }
+}
+
+const myQueue = new Queue();
+myQueue.enqueue(1);
+console.log('Front element:', myQueue.peek());  // logs 'Front element: 1'
+myQueue.enqueue(2);
+console.log('Front element:', myQueue.peek());  // logs 'Front element: 1'
+myQueue.enqueue(3);
+console.log('Front element:', myQueue.peek());  // logs 'Front element: 1'
+myQueue.dequeue();
+console.log('Front element after dequeue:', myQueue.peek());  // logs 'Front element after dequeue: 2'
+myQueue.dequeue();
+console.log('Front element after dequeue:', myQueue.peek());  // logs 'Front element after dequeue: 3'
+myQueue.dequeue();
+console.log('Peek on empty queue:', myQueue.peek());  // logs 'Peek on empty queue: null'
+console.log('`back` on empty queue:', myQueue.back);  // logs '`back` on empty queue: null'
 
 
 
