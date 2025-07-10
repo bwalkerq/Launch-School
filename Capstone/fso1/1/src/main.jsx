725563b7389fe8a4ac07946d8f6b4e2e381cbee3
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-07-10 15:26:10 -0500

    fso2 created, with file refactoring in Capstone

---
+++ Capstone/fso1/1/src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+import Anecdotes from './Anecdotes.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+    <App />
+    <Anecdotes/>
+  </div>
+)
