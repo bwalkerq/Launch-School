52005be6c5ecf270d0b6d1f4ea012180c6734a7d
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-07-14 14:01:13 -0500

    FSO 2c exercise complete

--- Capstone/FullStackOpen_React/phonebook/src/App.jsx
+++ Capstone/FullStackOpen_React/phonebook/src/App.jsx
@@ -1,20 +1,27 @@
 // App.js
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Filter from "./components/Filter.jsx";
 import PersonForm from './components/PersonForm.jsx'
 import Persons    from './components/Persons.jsx'
+import axios from "axios";
+
 
 const App = () => {
-  const [persons, setPersons]     = useState([
-    { name: 'Arto Hellas',      number: '040-123456',    id: 1 },
-    { name: 'Ada Lovelace',     number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov',      number: '12-43-234345',  id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons]       = useState([])
   const [newName, setNewName]       = useState('')
   const [newNumber, setNewNumber]   = useState('')
   const [filter, setFilter]         = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, []);
 
   const addPerson = e => {
     e.preventDefault()
