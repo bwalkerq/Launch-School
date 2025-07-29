38fc038da017a98bbaa4ee0aaf1c50b09d0cf97a
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-07-29 08:14:17 -0500

    Add initial Python script with max value calculation
    This commit introduces a Python script containing a basic implementation for finding the maximum value in a predefined list of numbers. It also includes a sample function to print a greeting and comments for IDE usage guidance.

---
+++ Capstone/Python/main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+max = '0'
+for number in ['10', '2', '34', '6', '25']:
+    print('max=', max, 'number=', number)
+    if int(number) > int(max):
+        max = number
+
+print('max value is', max)
