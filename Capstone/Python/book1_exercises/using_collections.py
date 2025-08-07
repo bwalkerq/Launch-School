df1400cb30786b9bc2809992dbfa3e49e7d1664e
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-08-07 15:41:51 -0500

    Add introductory Python exercises for book 1
    Created two new files, `intro_exercises.py` and `using_collections.py`, to include basic Python examples and practice problems. These exercises cover range operations, slicing, and string manipulations.

---
+++ Capstone/Python/book1_exercises/using_collections.py
@@ -0,0 +1,11 @@
+# Write Python code to print the seventh number of range(0, 25, 3).
+# a = range(0, 25, 3)
+# print(a[6])
+
+# b = 'Launch School'
+# print(b[4:10])
+# my_str = 'Launch School'
+# start = my_str.find('c')
+# print(my_str[start:start + 6])        # ch Sch
+
+
