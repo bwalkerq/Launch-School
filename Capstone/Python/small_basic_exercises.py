468631d2111a23406588cca00d2f388ea15e26e8
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-08-19 14:43:19 -0500

    python basic loop exercises

---
+++ Capstone/Python/small_basic_exercises.py
@@ -0,0 +1,75 @@
+## loops
+# for num in range(0, 11, 2):
+#     print(num)
+
+# for i in range(10,0,-1):
+#     print(i)
+# print("launch!")
+
+# greeting = 'Aloha!'
+# for _ in range(3):
+#     print(greeting)
+
+# for n in range(1,101):
+#     print(n*2)
+
+# lst = [1, 3, 7, 15]
+# index = 0
+#
+# while index < len(lst):
+#     print(lst[index])
+#     index += 1
+
+# friends = ['Sarah', 'John', 'Hannah', 'Dave']
+# for f in friends:
+#     print(f'Hello, {f}!')
+
+# cities = ['Istanbul', 'Los Angeles', 'Tokyo', None,
+#           'Vienna', None, 'London', 'Beijing', None]
+#
+# for city in cities:
+#     if city is None:
+#         continue
+#     print(city)
+
+# while True:
+#     print("and on")
+#     break
+
+# fish_list = ['Dory', 'Marlin', 'Gill', 'Nemo', 'Bruce']
+#
+# for f in fish_list:
+#     print(f)
+#     if f == 'Nemo':
+#         break
+
+## Conditionals
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
